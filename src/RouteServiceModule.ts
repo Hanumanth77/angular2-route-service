ac524256fac920177658cd6bde64f6e8f555b711
apoterenko <a.poterenko@gmail.com>
2016-08-31 11:24:12 +0300

    * replace RouteServiceModule with RouteServiceModuleFactory

--- src/RouteServiceModule.ts
+++ src/RouteServiceModule.ts
@@ -1,14 +1,39 @@
-import {NgModule} from '@angular/core';
+import {
+    NgModule,
+    Type
+} from '@angular/core';
 import * as ngCore from '@angular/core';
 
-import {NgRouteService, RouteService} from './RouteService';
+import {isBlank} from '@angular/core/src/facade/lang';
 
-@NgModule({
-    providers: [
-        ngCore.provide(RouteService, {useClass: NgRouteService})
-    ]
-})
-export class RouteServiceModule {
+import {NgRouteService, RouteService} from './RouteService';
+import {RouteDispatcher, IRouteDispatcher} from './RouteDispatcher';
+import {IAppStateRoute, AppStateRoute} from './RouteState';
+
+export class RouteServiceModuleFactory {
+
+    static makeModule(stateCtor?:{new (...args:Array<any>):IAppStateRoute}, dispatcherCtor?:{new (...args:Array<any>):IRouteDispatcher}):Type {
+        const providers:Array<any> = [
+            ngCore.provide(RouteService, {useClass: NgRouteService})
+        ];
+
+        providers.push(
+            !isBlank(stateCtor)
+                ? ngCore.provide(AppStateRoute, {useFactory: (state:IAppStateRoute) => state, deps: [stateCtor]})
+                : AppStateRoute
+        );
+
+        providers.push(
+            !isBlank(dispatcherCtor)
+                ? ngCore.provide(RouteDispatcher, {useFactory: (dispatcher:IRouteDispatcher) => dispatcher, deps: [dispatcherCtor]})
+                : RouteDispatcher
+        );
+
+        @NgModule({providers: providers})
+        class RouteServiceModule {
+        }
+        return RouteServiceModule;
+    }
 }
 
 // before Angular2 router code will be executed
