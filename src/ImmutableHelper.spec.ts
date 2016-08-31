e2db7ba82e968fc3003679b6293b6f56e43f4a12
apoterenko <a.poterenko@gmail.com>
2016-08-31 09:57:20 +0300

    *added tests

--- src/ImmutableHelper.spec.ts
+++ src/ImmutableHelper.spec.ts
@@ -1,8 +1,29 @@
+import {ImmutableHelper} from './ImmutableHelper';
+
 describe('ImmutableHelper', ()=> {
 
     describe('ImmutableHelper should work correctly', ()=> {
-
         it('Check cloning & freezing', ()=> {
+            const source = {
+                a: 1,
+                b: 2,
+                c: {
+                    d: 3,
+                    e: 4,
+                    f: {
+                        g: 5,
+                        h: 'hello!'
+                    }
+                }
+            };
+            const immutableObject = ImmutableHelper.toImmutable(source);
+
+            expect(immutableObject).toEqual(source);
+            try {
+                immutableObject['c']['f']['h'] = 'hello2';
+            } catch (e) {
+                expect(e.message).toBe('Attempted to assign to readonly property.');
+            }
         });
     });
 });
